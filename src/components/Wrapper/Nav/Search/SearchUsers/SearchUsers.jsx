d161ef5f0ae5afe03fbaed2594a83d7afad9b543
Anna86420 <aportnova864@gmail.com>
2026-03-15 18:52:37 +0400

    version 012: created a class component

--- src/components/Wrapper/Nav/Search/SearchUsers/SearchUsers.jsx
+++ src/components/Wrapper/Nav/Search/SearchUsers/SearchUsers.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import UserItem from './UserItem'
 import axios from 'axios'
-const SearchUsers = (props) => {
-  let getUsers = () => {
-    if (props.UsersData.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        props.setUsers(response.data.items)
-      })
-    }
+class SearchUsers extends React.Component {
+  componentDidMount() {
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      this.props.setUsers(response.data.items)
+    })
   }
-  let UsersElements = props.UsersData.map(user => (<UserItem
-    key={user.id}
-    name={user.name}
-    id={user.id}
-    addFriend={props.addFriend}
-    removeFriend={props.removeFriend}
-    isFriend={user.isFriend}
-    setUsers={props.setUsers}
-    photos={user.photos}
-  />))
-  return (
-    <div>
-      <button onClick={getUsers}>Get Users</button>
-      All Users:
-      {UsersElements}
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        All Users:
+        {
+          this.props.UsersData.map(user => (<UserItem
+            key={user.id}
+            name={user.name}
+            id={user.id}
+            addFriend={this.props.addFriend}
+            removeFriend={this.props.removeFriend}
+            isFriend={user.isFriend}
+            setUsers={this.props.setUsers}
+            photos={user.photos}
+          />))
+        }
+      </div>
+    )
+  }
 }
 export default SearchUsers
